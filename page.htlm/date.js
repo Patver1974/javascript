27d01e4a84e20ec5d2999fd6d3cb3053625e6d00
Patrick <patver1974@gmail.com>
2021-06-29 14:31:46 +0200

    date

--- page.htlm/date.js
+++ page.htlm/date.js
@@ -1,4 +1,12 @@
-const resultatSwitch = document.getElementById('resultat_switch');
-const d1 = new date
+const dateid = document.getElementById('dateid');
+
 const dateStrîng = prompt('Encoder une date DD-MM-AAAA');
-const d1 = new date
+if (dateStrîng.length==10) {
+    const jour = dateStrîng.substring(0,2);
+    const mois = dateStrîng.substring(3,2);
+    const annee = dateStrîng.substring(6,4);
+    const d1 = new date(parseInt(annee),parseInt(mois),parseInt(jour));
+    dateid.innerHTML = d1;
+
+
+}
