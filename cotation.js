9116a9af41c049c6c99c35d0bc35a01b86cb332a
Patrick <patver1974@gmail.com>
2021-06-29 14:06:54 +0200

    cotation

--- cotation.js
+++ cotation.js
@@ -1,3 +1,4 @@
+
 const resultatSwitch = document.getElementById('resultat_switch');
 const cotation = parseInt(prompt('Encoder une cotation de 1 a 20'));
 switch (cotation) {
@@ -11,19 +12,27 @@ switch (cotation) {
 
         resultatSwitch.innerHTML = 'Echec'; 
         break;
-    case 8-9: 
+    case 8:
+    case 9: 
         resultatSwitch.innerHTML = 'insuffisant'; 
         break;
-    case 3: 
+    case 10:
+    case 11:
+        case 12: 
         resultatSwitch.innerHTML = 'Suffisant'; 
         break;
-    case 4: 
+    case 13:
+    case 14: 
+    case 15: 
+    case 16:
         resultatSwitch.innerHTML = 'Bien'; 
         break;
-    case 5: 
+    case 17:
+    case 18:
+    case 19:
         resultatSwitch.innerHTML = 'Tres bien'; 
         break;
-    case 6: 
+    case 20: 
         resultatSwitch.innerHTML = 'Excellent'; 
         break;
 
---
+++ page.htlm/date.js
@@ -0,0 +1,4 @@
+const resultatSwitch = document.getElementById('resultat_switch');
+const d1 = new date
+const dateStrîng = prompt('Encoder une date DD-MM-AAAA');
+const d1 = new date
